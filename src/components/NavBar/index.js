23e0278548cdc293e11f7c2cfbbfe01da49a1def
Edward <edwardhhf0102@gmail.com>
2019-04-24 20:55:17 +0800

    [Component Create] NavBar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,58 @@
+import React, { PureComponent } from 'react';
+import styles from './nav-bar.module.scss';
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends PureComponent {
+  constructor (props) {
+    super(props);
+    this.state = {
+      active: 0,
+    }
+  }
+
+  static defaultProps = {
+    list: ['home', 'orders', 'product'],
+  }
+
+  handleClick = (idx) => () => {
+    this.setState({
+      active: idx,
+    });
+  }
+
+  itemStyle = (idx) => cx({
+    [styles['active']]: this.state.active === idx,
+  })
+
+  renderBrand = () => {
+    return (
+      <div className={styles['brand']} onClick={this.handleClick(0)}>
+        <Link to="/home">Shoptime</Link>
+      </div>
+    );
+  }
+
+  renderList = () => {
+    const { list } = this.props;
+    return (
+      <ul className={styles['list']}>
+        {list.map((item, idx) =>
+          <li key={idx} className={this.itemStyle(idx)} onClick={this.handleClick(idx)}>
+            <Link to={`/${item}`}>{item.toUpperCase()}</Link>
+          </li>
+        )}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className={styles['container']}>
+        {this.renderBrand()}
+        {this.renderList()}
+        <div className={styles['admin']}>Admin</div>
+      </div>
+    );
+  }
+}
