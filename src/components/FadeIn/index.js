b949450ee7202c03a33be3e10d18b9f8f26c34ae
Edward <edwardhhf0102@gmail.com>
2019-04-28 07:09:47 +0800

    [Component Create] FadeIn

---
+++ src/components/FadeIn/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../../stylesheets/animations.module.scss';
+
+const FadeIn = ({ tag: Tag, className, children }) => {
+  return (
+    <Tag className={styles[className]}>
+      {children}
+    </Tag>
+  );
+}
+
+FadeIn.defaultProps = {
+  tag: 'div',
+  className: 'fade-in',
+}
+
+export default FadeIn;
