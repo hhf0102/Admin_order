a372a7d20e3746379f3aac4bbc14e91bee75c268
Edward <edwardhhf0102@gmail.com>
2019-11-07 17:48:29 +0800

    refactor the component (ProductPage) for using hooks to access the specific state

--- src/components/ProductPage/TitleBar.js
+++ src/components/ProductPage/TitleBar.js
@@ -1,49 +1,44 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import styles from './title-bar.module.scss';
-import Button from 'components/Button';
-import CheckboxAndTag from 'components/CheckboxAndTag';
-import { selectStatusProductPage } from 'constants/selectStatus';
-import { changeStatusProductsPage } from 'constants/changeStatus';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './title-bar.module.scss'
+import Button from 'components/Button'
+import CheckboxAndTag from 'components/CheckboxAndTag'
+import { selectStatusProductPage } from 'constants/selectStatus'
+import { changeStatusProductsPage } from 'constants/changeStatus'
+import { useSelector, useDispatch } from 'react-redux'
+import { clickAllChecked, selectArrowOption, changeStatus, clickAddNewProduct } from 'actions/productsPage'
 
+const TitleBar = () => {
+  const dispatch = useDispatch()
+  const isAllChecked = useSelector(state => state.products.titleBarCheckBoxStatus)
+  const handleChangeAllChecked = status => () => dispatch(clickAllChecked(status))
+  const handleSelectArrow = checked => () => dispatch(selectArrowOption(checked))
+  const handleChangeStatus = status => () => dispatch(changeStatus(status))
+  const handleClickAddNewProduct = dialogName => () => dispatch(clickAddNewProduct(dialogName))
 
-export default class TitleBar extends PureComponent {
-  static propTypes = {
-    isAllChecked: PropTypes.bool,
-    handleChangeAllChecked: PropTypes.func,
-    handleSelectArrow: PropTypes.func,
-    handleChangeStatus: PropTypes.func,
-    handleClickAddNewProduct: PropTypes.func,
-  }
-
-  renderBtn = () => {
-    const { handleClickAddNewProduct } = this.props
-    return (
+  return (
+    <div className={styles['title-wrapper']}>
+      <CheckboxAndTag
+        isAllChecked={isAllChecked}
+        handleChangeAllChecked={handleChangeAllChecked}
+        handleSelectArrow={handleSelectArrow}
+        handleChangeStatus={handleChangeStatus}
+        arrowList={selectStatusProductPage}
+        tagList={changeStatusProductsPage}
+      />
       <div className={styles['btn-wrapper']} onClick={handleClickAddNewProduct({ dialogName: 'addNewProduct' })}>
-        <Button btnText="add new product" addItem />
+        <Button btnText='add new product' addItem />
       </div>
-    );
-  }
-
-  render () {
-    const {
-      isAllChecked,
-      handleChangeAllChecked,
-      handleSelectArrow,
-      handleChangeStatus,
-    } = this.props;
-    return (
-      <div className={styles['title-wrapper']}>
-        <CheckboxAndTag
-          isAllChecked={isAllChecked}
-          handleChangeAllChecked={handleChangeAllChecked}
-          handleSelectArrow={handleSelectArrow}
-          handleChangeStatus={handleChangeStatus}
-          arrowList={selectStatusProductPage}
-          tagList={changeStatusProductsPage}
-        />
-        {this.renderBtn()}
-      </div>
-    );
-  }
+    </div>
+  )
 }
+
+TitleBar.propTypes = {
+  isAllChecked: PropTypes.bool,
+  handleChangeAllChecked: PropTypes.func,
+  handleSelectArrow: PropTypes.func,
+  handleChangeStatus: PropTypes.func,
+  handleClickAddNewProduct: PropTypes.func
+}
+
+export default TitleBar
--- src/components/ProductPage/index.js
+++ src/components/ProductPage/index.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import styles from './product-page.module.scss';
-import FadeIn from 'components/FadeIn';
-import TitleBar from 'containers/TitleBarProducts';
-import Table from 'containers/TableProducts';
-import AddNewProductModel from 'containers/AddNewProductModel';
-import Dialog from 'containers/Dialog';
+import React from 'react'
+import styles from './product-page.module.scss'
+import FadeIn from 'components/FadeIn'
+import TitleBar from 'components/ProductPage/TitleBar'
+import Table from 'containers/TableProducts'
+import AddNewProductModel from 'containers/AddNewProductModel'
+import Dialog from 'containers/Dialog'
 
 const ProductPage = () => {
   return (
     <FadeIn>
-      <div className={styles["container"]}>
+      <div className={styles['container']}>
         <TitleBar />
         <Table />
-        <Dialog
-          name="addNewProduct"
-          component={AddNewProductModel}
-        />
+        <Dialog name='addNewProduct' component={AddNewProductModel} />
       </div>
     </FadeIn>
   )
-};
+}
 
-export default ProductPage;
+export default ProductPage
