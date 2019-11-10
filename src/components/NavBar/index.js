04856fd8e067948116ba79b7f96154d05a470e9a
Edward <edwardhhf0102@gmail.com>
2019-11-10 19:14:14 +0800

    Fix the bug of route

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,62 +1,52 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import styles from './nav-bar.module.scss';
-import cx from 'classnames';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import styles from './nav-bar.module.scss'
+import cx from 'classnames'
+import { Link } from 'react-router-dom'
 
-export default class NavBar extends PureComponent {
-  static propTypes = {
-    list: PropTypes.array,
-  }
-  
-  state = {
-    active: 0,
-  }
+const NavBar = () => {
+  const list = ['home', 'orders', 'product']
+  const [active, setActive] = useState(list.findIndex(route => location.hash.includes(route)))
 
-  static defaultProps = {
-    list: ['home', 'orders', 'product'],
-  }
+  const itemStyle = idx =>
+    cx({
+      [styles['active']]: idx === active
+    })
 
-  handleClick = (idx) => () => {
-    this.setState({
-      active: idx,
-    });
-  }
+  const handleClick = () => setActive(list.findIndex(route => location.hash.includes(route)))
 
-  itemStyle = (idx) => cx({
-    [styles['active']]: this.state.active === idx,
-  })
-
-  renderBrand = () => {
+  const renderBrand = () => {
     return (
-      <div className={styles['brand']} onClick={this.handleClick(0)}>
-        <Link to="/home">Shoptime</Link>
+      <div className={styles['brand']} onClick={handleClick}>
+        <Link to='/home'>Shoptime</Link>
       </div>
-    );
+    )
   }
 
-  renderList = () => {
-    const { list } = this.props;
+  const renderList = () => {
     return (
       <ul className={styles['list']}>
         {list.map((item, idx) => (
-          <li key={idx} className={this.itemStyle(idx)} onClick={this.handleClick(idx)}>
+          <li key={idx} className={itemStyle(idx)} onClick={handleClick}>
             <Link to={`/${item}`}>{item.toUpperCase()}</Link>
           </li>
         ))}
       </ul>
     )
   }
-
-  render() {
-    return (
-      <div className={styles['container']}>
-        <div className={styles['wrapper']}>
-          {this.renderBrand()}
-          {this.renderList()}
-          <div className={styles['admin']}>Admin</div>
-        </div>
+  return (
+    <div className={styles['container']}>
+      <div className={styles['wrapper']}>
+        {renderBrand()}
+        {renderList()}
+        <div className={styles['admin']}>Admin</div>
       </div>
-    );
-  }
+    </div>
+  )
 }
+
+NavBar.propTypes = {
+  list: PropTypes.array
+}
+
+export default NavBar
