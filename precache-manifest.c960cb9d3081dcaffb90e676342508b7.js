7131ff06ccc6cdc19ba6fe5494a375b03dd08614
Edward <edwardhhf0102@gmail.com>
2020-11-15 11:19:07 +0800

    Updates

---
+++ precache-manifest.c960cb9d3081dcaffb90e676342508b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2de85a78d233c532099bdbe4b965706",
+    "url": "/Admin_order/static/media/shirt.f2de85a7.jpg"
+  },
+  {
+    "revision": "ef9d991b39895698a4fe3e19572b1270",
+    "url": "/Admin_order/static/media/jacket.ef9d991b.jpg"
+  },
+  {
+    "revision": "34790902838a556031c5cc589796f351",
+    "url": "/Admin_order/static/media/coat.34790902.jpg"
+  },
+  {
+    "revision": "8e064a737e9a5eb5a0855f0ee0045adf",
+    "url": "/Admin_order/static/media/checkmark.8e064a73.svg"
+  },
+  {
+    "revision": "0c0ed53d3653e883f7a8274f431c2fbd",
+    "url": "/Admin_order/static/media/chart.0c0ed53d.jpg"
+  },
+  {
+    "revision": "310555de737ea0d55d4b",
+    "url": "/Admin_order/static/js/runtime~main.cfdb8984.js"
+  },
+  {
+    "revision": "95577703468034d79f05",
+    "url": "/Admin_order/static/js/main.d4c701fc.chunk.js"
+  },
+  {
+    "revision": "dbeecc351d6a1978fab8",
+    "url": "/Admin_order/static/js/2.4304014c.chunk.js"
+  },
+  {
+    "revision": "95577703468034d79f05",
+    "url": "/Admin_order/static/css/main.639b719a.chunk.css"
+  },
+  {
+    "revision": "0a4fd3003c6d59dd808882bde4910279",
+    "url": "/Admin_order/index.html"
+  }
+];
